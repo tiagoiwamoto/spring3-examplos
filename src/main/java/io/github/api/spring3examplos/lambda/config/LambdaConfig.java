e084044faeb30c797a63dca160587e6a9699e849
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2025-05-24 07:32:25 -0300

    dockercompose

--- src/main/java/io/github/api/spring3examplos/lambda/config/LambdaConfig.java
+++ src/main/java/io/github/api/spring3examplos/lambda/config/LambdaConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 
@@ -16,15 +17,23 @@ public class LambdaConfig {
 
     @Value(value = "${app.aws.region}")
     private String region;
+    @Value(value = "${app.aws.environment}")
+    private String environment;
 
     @Bean
     public LambdaClient lambdaClient() {
         log.info("Criando cliente Lambda");
-        return LambdaClient.builder()
-                .endpointOverride(URI.create("http://localhost:4566"))
-                .region(Region.of(region))
-                .credentialsProvider(AnonymousCredentialsProvider.create())
-                .build();
+        var lambdaBuilder = LambdaClient.builder()
+                .region(Region.of(region));
+
+        if("local".equals(environment)) {
+            lambdaBuilder.endpointOverride(URI.create("http://localhost:4566"));
+            lambdaBuilder.credentialsProvider(AnonymousCredentialsProvider.create());
+        }else{
+            lambdaBuilder.credentialsProvider(DefaultCredentialsProvider.create());
+        }
+
+        return lambdaBuilder.build();
     }
 
 }
